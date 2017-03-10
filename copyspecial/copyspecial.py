cf02a68b808af90c4054097254b2cd90ae92befd
Michael Berber <mickberber@icloud.com>
2017-03-10 13:09:04 -0800

    complete copyspecial todir

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -26,7 +26,23 @@ def get_files(dir):
             path = os.path.join(dir, filename)
             abspath = os.path.abspath(path)
             paths.append(abspath)
-    print paths
+    return paths
+
+def to_directory(files, destination):
+    destpath = os.path.abspath(destination)
+    mkdir_cmd = 'mkdir ' + destpath
+    (status, output) = commands.getstatusoutput(mkdir_cmd)
+    if status:
+        sys.stderr.write('there was an error:' + output)
+        sys.exit(1)
+
+    for f in files:
+        cp_cmd = 'cp ' + f + ' ' + destpath
+        (st, op) = commands.getstatusoutput(cp_cmd)
+        if st:
+            sys.stderr.write('there was an error:' + op)
+            sys.exit(1)
+    return
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -42,23 +58,24 @@ def main():
   # todir and tozip are either set from command line
   # or left as the empty string.
   # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+  if args[0] == '--todir' and len(args) == 3:
+      source = args[1]
+      destination = args[2]
+      files = get_files(source)
+      to_directory(files, destination)
+      return
 
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+  if args[0] == '--tozip' and len(args) == 3:
+      source = args[1]
+      destination = args[2]
+      return
 
   if len(args) == 0:
     print "error: must specify one or more dirs"
     sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
-  get_files(args[0])
+  print get_files(args[0])
+  return
 
 if __name__ == "__main__":
   main()
