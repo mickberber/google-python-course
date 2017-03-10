5d0b1194fadfb284e5aa8836eec3c029f24ac528
Michael Berber <mickberber@icloud.com>
2017-03-09 17:16:17 -0800

    add to wordcount.py

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -44,8 +44,34 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+def count_words(filename):
+    lib = {}
+    f = open(filename, 'rU')
+    for line in f:
+        line.lower()
+        words = line.split(' ')
+        for word in words:
+            if lib.get(word):
+                lib[word] += 1
+            else:
+                lib[word] = 1
+    f.close()
+    return lib
 
-###
+def print_top(filename):
+    words = count_words(filename)
+    def keyfunc(tupe):
+        return tupe[1]
+    words = sorted(words.items(), key=keyfunc, reverse=True)
+    i = 0
+    while i < 20:
+        print words[i][0], words[i][1]
+        i += 1
+
+def print_words(filename):
+    words = count_words(filename)
+    for word in words.items():
+        print word[0], word[1]
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
